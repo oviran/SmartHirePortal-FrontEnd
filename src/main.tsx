9abddac67764ef998c1de32eb584ab0c67f980ec
Viran <oviran@gmail.com>
2024-04-16 20:45:37 +0530

    home

--- src/main.tsx
+++ src/main.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element: <MainLayout />,
         children: [
           {
-            path: "/",
+            path: "/home",
             element: <HomePage />,
           },
           {
