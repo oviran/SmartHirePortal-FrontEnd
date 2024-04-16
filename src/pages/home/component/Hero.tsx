7c0111f9f99fdde4c0717fc2018734c2d3843e99
Viran <oviran@gmail.com>
2024-04-16 21:32:33 +0530

    vbv

--- src/pages/home/component/Hero.tsx
+++ src/pages/home/component/Hero.tsx
@@ -3,7 +3,7 @@ function Hero() {
   return (
     <section className="container py-44 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero ">
       <div>
-        <h1 className= "container text-underlay-2 absolute right-28 mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
+        <h1 className= "container text-underlay-2 absolute right-32 mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
         Realize Your Career Goals
         </h1>
       </div>
