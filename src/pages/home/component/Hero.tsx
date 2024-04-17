ca13100ad8669b75a10cdfe37ba19bbab1143b3a
Viran <oviran@gmail.com>
2024-04-17 11:25:36 +0530

    dfad

--- src/pages/home/component/Hero.tsx
+++ src/pages/home/component/Hero.tsx
@@ -8,8 +8,8 @@ function Hero() {
   return (
     <section className=" py-44 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero ">
       <div >
-        <TextFit mode="single" max={32}>
-          <h1 className= " text-underlay-2 absolute  mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
+        <TextFit mode="single" max={40}>
+          <h1 className= " text-underlay-2 absolute right-28 mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
           Realize Your Career Goals
           </h1>
         </TextFit>
