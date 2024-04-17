25fcf0aad3da93d4e82bb4bcc01c4f1dc9078876
Viran <oviran@gmail.com>
2024-04-17 11:38:01 +0530

    ss

--- src/pages/home/component/Hero.tsx
+++ src/pages/home/component/Hero.tsx
@@ -1,26 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import TextFit from 'react-textfit';
-
-
-
 function Hero() {
   return (
     <section className=" py-44 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero ">
       <div >
-        <TextFit mode="single" max={40}>
-          <h1 className= " text-underlay-2 absolute right-28 mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
+        <h1 className="text-underlay-2 absolute right-28 md:right-auto mt-[-110px] md:mt-0 text-4xl md:text-6xl font-serif animate-pulse antialiased">
           Realize Your Career Goals
-          </h1>
-        </TextFit>
+        </h1>
       </div>
     </section>
   );
 }
 
-export default Hero;  
-
-
-
-
-
+export default Hero;
