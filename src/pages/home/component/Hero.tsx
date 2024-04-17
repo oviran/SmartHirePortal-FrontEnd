e106a74b6eba6239bf6f17f94c3444b053d9cd67
Viran <oviran@gmail.com>
2024-04-17 11:39:40 +0530

    mnb

--- src/pages/home/component/Hero.tsx
+++ src/pages/home/component/Hero.tsx
@@ -2,7 +2,7 @@ function Hero() {
   return (
     <section className=" py-44 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero ">
       <div >
-        <h1 className="text-underlay-2 absolute right-28 md:right-auto mt-[-110px] md:mt-0 text-4xl md:text-6xl font-serif animate-pulse antialiased">
+        <h1 className="text-underlay-2  md:right-auto  md:mt-0 md:text-6xl font-serif animate-pulse antialiased">
           Realize Your Career Goals
         </h1>
       </div>
