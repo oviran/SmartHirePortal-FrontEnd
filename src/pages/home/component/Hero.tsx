9147a06a98a5d48da9975ae34d327af45bb4c418
Viran <oviran@gmail.com>
2024-04-17 11:13:35 +0530

    ho

--- src/pages/home/component/Hero.tsx
+++ src/pages/home/component/Hero.tsx
@@ -1,9 +1,9 @@
 
 function Hero() {
   return (
-    <section className="container py-44 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero ">
-      <div>
-        <h1 className= "container text-underlay-2 absolute right-32 mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
+    <section className=" py-44 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900 border-white hero ">
+      <div className="container ">
+        <h1 className= " text-underlay-2 absolute right-32 mt-[-110px] text-6xl font-serif animate-pulse antialiased  "  > 
         Realize Your Career Goals
         </h1>
       </div>
@@ -11,7 +11,7 @@ function Hero() {
   );
 }
 
-export default Hero;
+export default Hero;  
 
 
 
--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/clerk-react";
 function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <SignUp afterSignInUrl={"/home"} signInUrl="/sign-in"/>
+      <SignUp afterSignInUrl={"home"} signInUrl="/sign-in"/>
     </div>
   );
 }
